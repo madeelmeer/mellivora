964b864651b16bc1133b5222f9d90ec3c78231e9
madeelmeer <adeelmir@hotmail.com>
2018-07-26 14:10:57 +0100

    Add files via upload

--- include/email.inc.php
+++ include/email.inc.php
@@ -142,6 +142,7 @@ function allowed_email ($email) {
 }
 
 function valid_email ($email) {
+	$email = filter_var($email, FILTER_SANITIZE_EMAIL);
     return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
 
